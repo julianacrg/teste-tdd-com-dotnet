3582db0c1998297aee170dc9799811aaf2955ca0
julianacrg <julianacrgomes@gmail.com>
2023-11-10 16:26:08 -0300

    Criacao do codigo de desenvolvimento

--- Calculadora/Services/CalculadoraImp.cs
+++ Calculadora/Services/CalculadoraImp.cs
@@ -7,29 +7,53 @@ namespace Calculadora.Services
 {
     public class CalculadoraImp
     {
+        private List<string> listaHistorico;
+
+        public CalculadoraImp()
+        {
+            listaHistorico = new List<string>();
+        }
+
         public int Somar (int val1, int val2)
         {
-            return 0;
+            int resultado = val1 + val2;
+
+            listaHistorico.Insert(0, $"{val1} + {val2} = {resultado}");
+
+            return resultado;
         }
 
         public int Subtrair (int val1, int val2)
         {
-            return 0;
+            int resultado = val1 - val2;
+
+            listaHistorico.Insert(0, $"{val1} - {val2} = {resultado}");
+
+            return resultado;
         }
 
         public int Multiplicar (int val1, int val2)
         {
-            return 0;
+            int resultado = val1 * val2;
+
+            listaHistorico.Insert(0, $"{val1} * {val2} = {resultado}");
+
+            return resultado;
         }
 
         public int Dividir (int val1, int val2)
         {
-            return 0;
+            int resultado = val1 / val2;
+
+            listaHistorico.Insert(0, $"{val1} / {val2} = {resultado}");
+
+            return resultado;
         }
 
         public List<string> Historico ()
         {
-            return new List<string>();
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
         }
     }
 }
